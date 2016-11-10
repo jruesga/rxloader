b307d5e822eaf8af775c01cae12432929dbafcc9
Jorge Ruesga <jorge@ruesga.com>
2016-11-11 00:20:21 +0100

    Add helper classes to create safe observables
    In some rxjava classes if a subcription was disposed before
    an onError happens, instead of silent catch the exception,
    rxjava raises the exception, which causes a FC since the
    exception was converted into an uncatched exception.
    
    This patchse provides a SafeObservable.fromXXX methods
    to create safe observables that doesn't raise uncatched
    exceptions in case the subscription was disposed.

---
+++ rxloader2/src/main/java/me/tatarka/rxloader2/safe/SafeObservable.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2016 Jorge Ruesga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.tatarka.rxloader2.safe;
+
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.annotations.SchedulerSupport;
+import io.reactivex.internal.functions.ObjectHelper;
+import io.reactivex.plugins.RxJavaPlugins;
+
+/**
+ * An observable helper to create safe observables that doesn't throw uncatched exceptions.
+ */
+public class SafeObservable {
+
+    @SchedulerSupport(SchedulerSupport.NONE)
+    public static <T> Observable<T> fromCallable(Callable<? extends T> supplier) {
+        ObjectHelper.requireNonNull(supplier, "supplier is null");
+        return RxJavaPlugins.onAssembly(new SafeObservableFromCallable<T>(supplier));
+    }
+
+}
---
+++ rxloader2/src/main/java/me/tatarka/rxloader2/safe/SafeObservableFromCallable.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2016 Jorge Ruesga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.tatarka.rxloader2.safe;
+
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.exceptions.Exceptions;
+import io.reactivex.internal.functions.ObjectHelper;
+import io.reactivex.internal.observers.DeferredScalarDisposable;
+
+/**
+ * This is a version from {@link io.reactivex.internal.operators.observable.ObservableFromCallable}
+ * that doesn't raise exceptions when subscription is disposed (otherwise exceptions
+ * can not be consumed, and are directly send to the vm).
+ */
+public final class SafeObservableFromCallable<T> extends Observable<T> implements Callable<T> {
+    final Callable<? extends T> callable;
+    public SafeObservableFromCallable(Callable<? extends T> callable) {
+        this.callable = callable;
+    }
+    @Override
+    public void subscribeActual(Observer<? super T> s) {
+        DeferredScalarDisposable<T> d = new DeferredScalarDisposable<T>(s);
+        s.onSubscribe(d);
+        if (d.isDisposed()) {
+            return;
+        }
+        T value;
+        try {
+            value = ObjectHelper.requireNonNull(callable.call(), "Callable returned null");
+        } catch (Throwable e) {
+            Exceptions.throwIfFatal(e);
+            if (!d.isDisposed()) {
+                s.onError(e);
+            }
+            return;
+        }
+        d.complete(value);
+    }
+
+    @Override
+    public T call() throws Exception {
+        return callable.call();
+    }
+}
+
